c74722b0cb617ab1a1f335aa484715ab7e5ad4cd
WasimHamid <wasim.hamid45@gmail.com>
2020-09-10 20:51:43 +0100

    added project page

--- App.js
+++ App.js
@@ -2,18 +2,26 @@ import 'react-native-gesture-handler'
 import React from 'react';
 import Explore from './screens/Explore';
 import {
-  View, Text, Button
+  View, Text, Button, StyleSheet
 } from "react-native";
 
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
+
 function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button title="Go to details" onPress={() => navigation.navigate('Details')} />
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+          Junior Developer
+        </Text>
+        <Text style={styles.welcome}>
+          React | JavaScript | HTML | CSS | Express | MongoDB | React Native
+        </Text>
+      </View>
+      <Button title="Go to Projects" onPress={() => navigation.navigate('Details')} />
     </View>
   )
 }
@@ -23,12 +31,31 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Wasim Hamid" component={HomeScreen} />
         <Stack.Screen name="Details" component={Explore} />
-      </Stack>
-      <Explore></Explore>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000'
+  },
+  title: {
+    fontSize: 35,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff'
+  },
+  welcome: {
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff',
+  }
+}); 
--- screens/Explore.js
+++ screens/Explore.js
@@ -1,19 +1,64 @@
 import React from "react";
 import {
-    StyleSheet,
-    SafeAreaView,
-    StatusBar
+    Image, Linking
 } from "react-native";
-
-import Home from "./components/Explore/Home";
+import {
+    Container, Content, Card, CardItem,
+    Text, Button, Icon, Left, Body, Right
+}
+    from 'native-base';
 
 class Explore extends React.Component {
+
+    state = {
+        likes: 10
+    };
+
+    addLike = () => {
+        let newCount = this.state.likes + 1;
+        this.setState({
+            likes: newCount
+        });
+    };
+
     render() {
         return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <StatusBar hidden />
-                <Home></Home>
-            </SafeAreaView>
+            <Container>
+                <Content>
+                    <Card>
+                        <CardItem>
+                            <Left>
+                                <Body>
+                                    <Text>Playermade</Text>
+                                    <Text note>www.playermade.club</Text>
+                                </Body>
+                            </Left>
+                        </CardItem>
+                        <CardItem cardBody>
+                            <Image source={require("../assets/Playermade.png")} style={{ height: 400, width: null, flex: 1 }} />
+                        </CardItem>
+                        <CardItem>
+                            <Left>
+                                <Button transparent onPress={() => this.addLike()}>
+                                    <Icon active name="thumbs-up" />
+                                </Button>
+                                <Text>{this.state.likes}</Text>
+                            </Left>
+                            <Body>
+                                <Button transparent>
+                                    <Icon active name="heart" style={{ color: 'red' }} />
+                                </Button>
+                            </Body>
+                            <Right>
+                                <Text style={{ color: 'blue', textDecorationLine: 'underline' }}
+                                    onPress={() => Linking.openURL('http://playermade.club')}>
+                                    Check it Out
+                                </Text>
+                            </Right>
+                        </CardItem>
+                    </Card>
+                </Content>
+            </Container>
         );
     }
 }
